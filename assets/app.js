da94568838f8d7e9cd2b93bb6165f55e5073af7f
Kimberly Calderon <kimberlycalderon@Kimberlys-MacBook-Pro.local>
2016-10-18 11:34:03 -0400

    js

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+//Computer choices
+var compChoices = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var computerPicks = compChoices[Math.floor(Math.random() * compChoices.length)];
+
+var yourGuessesLeft = function() {
+	
+}
