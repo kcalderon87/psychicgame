d9db448cbf25f4de7e425ecc48bd75d8acea0250
Kimberly Calderon <kimberlycalderon@Kimberlys-MacBook-Pro.local>
2016-10-19 17:32:02 -0400

    removed nonworking code

--- assets/app.js
+++ assets/app.js
@@ -20,49 +20,36 @@ var reset = function() {
   totalGuesses = 9;
   guessesLeft = 9;
   guessedLetters = [];
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
+  updatedGuess();
+  soFarGuessesLeft();
+  GuessesSoFar();
 };
 
-var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+var soFarGuessesLeft = function() {
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
-var updateLetterToGuess = function() {
+var updatedGuess = function() {
   this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
 
-var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+var GuessesSoFar = function() {
   document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
-
-updateLetterToGuess();
-updateGuessesLeft();
 
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
 
         if (guessesLeft > 0){
             if (userGuess == letterToGuess){
                 wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes, the letter was " + computerGuess.toUpperCase());
+                document.querySelector('wins').innerHTML = "Wins: " + wins;
                 reset();
             }
         }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
             losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Sorry, the correct letter was " + computerGuess.toUpperCase() + " maybe try again?");
-            // Then we'll call the reset. 
-            reset();
+            document.querySelector('losses').innerHTML = "Losses: " + losses;
         }
 };
