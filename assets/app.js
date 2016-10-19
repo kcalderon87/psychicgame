c2ce3a253270dd068a8b178eac46ff829adf7b9a
Kimberly Calderon <kimberlycalderon@Kimberlys-MacBook-Pro.local>
2016-10-19 17:25:04 -0400

    updated js

--- assets/app.js
+++ assets/app.js
@@ -4,5 +4,65 @@ var compChoices = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 var computerPicks = compChoices[Math.floor(Math.random() * compChoices.length)];
 
 var yourGuessesLeft = function() {
-	
-}
+	document.querySelector('#guessRemaining').innerHTML = "Guesses Left: " + guessRemaining;
+};
+
+//user can have 9 guesses
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+};
+
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, the letter was " + computerGuess.toUpperCase());
+                reset();
+            }
+        }else if(guessesLeft == 0){
+            // Then we will loss and we'll update the html to display the loss 
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, the correct letter was " + computerGuess.toUpperCase() + " maybe try again?");
+            // Then we'll call the reset. 
+            reset();
+        }
+};
